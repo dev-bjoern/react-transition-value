8f05e00e2f10131fee46a7bfb3e62e5d3547bbfd
Björn <bjn.moritz@gmail.com>
2022-04-19 17:19:34 +0200

    use CodeSandbox for demos

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import './App.css'
-import AutoStart from './examples/AutoStart'
-import Basic from './examples/Basic'
-import Scroll from './examples/Scroll'
 import packageJson from '../package.json'
 import { ReactComponent as GithubSvg } from './assets/github.svg'
 
@@ -12,29 +9,35 @@ const App = () => {
     <a
       style={{ display: "flex", margin: "auto", marginTop: 50 }}
       href={packageJson.repository.url}>
-      {packageJson.name} on <GithubSvg height={16} fill="white" style={{ margin: "auto", marginLeft: 4 }} /> Github  →</a>
+      {packageJson.name} on <GithubSvg height={16} fill="white" style={{ margin: "auto", marginLeft: 4 }} /> Github  →
+    </a>
 
     <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap", justifyContent: "space-evenly", marginTop: 30, paddingBottom: 30, maxWidth: 1600, marginLeft: "auto", marginRight: "auto" }}>
 
-      {/* {window.location.pathname === "/" && */}
-      <>
-        <Basic style={{ margin: 30 }} />
-        <AutoStart style={{ margin: 30 }} />
-        <Scroll style={{ margin: 30 }} />
-      </>
-      {/* } */}
+      <h1>Basic Example</h1>
+      <iframe src="https://codesandbox.io/embed/white-wind-it1t40?fontsize=14&hidenavigation=1&theme=dark"
+        style={{ width: "100%", height: "500px", border: "1px solid #292B32", borderRadius: "4px", overflow: "hidden" }}
+        title="white-wind-it1t40"
+        allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+        sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+      ></iframe>
 
-      {window.location.pathname === "/basic" &&
-        <Basic style={{ marginTop: 30 }} />
-      }
+      <h1 style={{ marginTop: 60 }}>Autostart Loop Example</h1>
+      <iframe src="https://codesandbox.io/embed/intelligent-sun-1kr0tb?fontsize=14&hidenavigation=1&theme=dark"
+        style={{ width: "100%", height: "500px", border: "1px solid #292B32", borderRadius: "4px", overflow: "hidden" }}
+        title="intelligent-sun-1kr0tb"
+        allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+        sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+      ></iframe>
 
-      {window.location.pathname === "/autoStart" &&
-        <AutoStart style={{ marginTop: 30 }} />
-      }
 
-      {window.location.pathname === "/scroll" &&
-        <Scroll style={{ marginTop: 30 }} />
-      }
+      <h1 style={{ marginTop: 60 }}>Scroll Example</h1>
+      <iframe src="https://codesandbox.io/embed/summer-snowflake-5svkje?fontsize=14&hidenavigation=1&theme=dark"
+        style={{ width: "100%", height: "500px", border: "1px solid #292B32", borderRadius: "4px", overflow: "hidden" }}
+        title="summer-snowflake-5svkje"
+        allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+        sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+      ></iframe>
     </div>
   </div>
 }
